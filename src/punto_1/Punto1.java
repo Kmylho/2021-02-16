072dae9492c3505d4bb5901047ac0b752db863f0
Kmylho <azprodiyi@gmail.com>
2021-02-16 16:51:24 -0500

    taller de java basico

--- src/punto_1/Punto1.java
+++ src/punto_1/Punto1.java
@@ -1,5 +1,6 @@
 package punto_1;
 
+import java.util.InputMismatchException;
 import java.util.Scanner;
 /**
  *
@@ -9,13 +10,24 @@ public class Punto1 {
 
     public static void main(String[] args) {
         Scanner reader = new Scanner(System.in);
-        System.out.println("Ingrese el primer numero entero");
-        int numero1 = reader.nextInt();
-        System.out.println("Ingrese el segundo numero entero");
-        int numero2 = reader.nextInt();
 
-
-        System.out.println("hello");
+        try{
+            System.out.println("Ingrese el primer numero entero");
+            int numero1 = reader.nextInt();
+            System.out.println("Ingrese el segundo numero entero");
+            int numero2 = reader.nextInt();
+            if (numero1>numero2){
+                System.out.println("El primer numero : "+numero1+" es mayor al segundo numero :"+numero2);
+            }else if(numero1<numero2){
+                System.out.println("El segundo numero : "+numero2+" es mayor al primer numero  :"+numero1);
+            }else if(numero1 == numero2){
+                System.out.println("Son iguales los numeros : "+numero1);
+            }else{
+                System.out.println("Error");
+            }
+        }catch (InputMismatchException e){
+            System.out.println("Caracter mal ingresado");
+        }
 
     }
 }
