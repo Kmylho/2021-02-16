b9c7259d68c82a7d15025d40609d79e82bcdbc09
Kmylho <azprodiyi@gmail.com>
2021-02-16 18:07:00 -0500

    punto 4 terminado

---
+++ src/punto_4/Punto_4.java
@@ -0,0 +1,15 @@
+package punto_4;
+
+import java.util.Scanner;
+
+public class Punto_4 {
+    static Scanner reader = new Scanner(System.in);
+
+    public static void main(String[] args) {
+        double iva = 21;
+        System.out.println("Ingrese el valor del precio");
+        double preciosiniva = reader.nextInt();
+
+        System.out.println(((iva*preciosiniva)/100)+preciosiniva);
+    }
+}
