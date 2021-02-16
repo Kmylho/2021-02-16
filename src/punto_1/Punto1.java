53bec37c2eaefab18cda2f079d4ddc852fabe656
Kmylho <azprodiyi@gmail.com>
2021-02-16 16:06:42 -0500

    taller de java basico

---
+++ src/punto_1/Punto1.java
@@ -0,0 +1,22 @@
+package punto_1;
+
+import java.util.Scanner;
+/**
+ *
+ * @author Kmylho https://linktr.ee/Kmylho
+ */
+public class Punto1 {
+
+    public static void main(String[] args) {
+        Scanner reader = new Scanner(System.in);
+        System.out.println("Ingrese el primer numero entero");
+        int numero1 = reader.nextInt();
+        System.out.println("Ingrese el segundo numero entero");
+        int numero2 = reader.nextInt();
+
+
+        System.out.println("hello");
+
+    }
+}
+
